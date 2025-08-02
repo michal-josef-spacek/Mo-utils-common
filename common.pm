bf9464e1928deeb491d2261d69a81dba0c958704
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-08-02 12:45:38 +0200

    Remove check_percent()

--- common.pm
+++ common.pm
@@ -7,46 +7,9 @@ use warnings;
 use Error::Pure qw(err);
 use Readonly;
 
-Readonly::Array our @EXPORT_OK => qw(check_percent);
+Readonly::Array our @EXPORT_OK => qw();
 
 our $VERSION = 0.01;
-
-sub check_percent {
-	my ($value, $key, $func, $error_value) = @_;
-
-	if (! defined $error_value) {
-		$error_value = $value;
-	}
-
-	if (! defined $func) {
-		$func = 'percent value';
-	}
-
-	# Check percent sign.
-	if ($value =~ m/^(\d+)(\%)?$/ms) {
-		$value = $1;
-		my $p = $2;
-		if (! $p) {
-			err "Parameter '$key' has bad $func (missing %).",
-				'Value', $error_value,
-			;
-		}
-	# Check percent number.
-	} else {
-		err "Parameter '$key' has bad $func.",
-			'Value', $error_value,
-		;
-	}
-
-	# Check percent value.
-	if ($value > 100) {
-		err "Parameter '$key' has bad $func.",
-			'Value', $error_value,
-		;
-	}
-
-	return;
-}
 
 1;
 
